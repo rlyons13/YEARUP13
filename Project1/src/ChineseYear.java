5b6742d959f1d2c3a63b6e40e4fcbc981484136f
rlyons13 <rlyons@atl.yearup.org>
2015-05-25 20:14:31 -0400

    Switch Statements Lab

---
+++ Project1/src/ChineseYear.java
@@ -0,0 +1,38 @@
+
+public class ChineseYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int s_knownYear = 1900;
+		int yearDifference =(unknownYear - s_knownYear)
+		int remainder = yearDifference % 12;
+		
+		System.out.println (s_knownYear + "");
+		
+	}
+	
+	public void evauateChineseYear();
+	return;
+}
+	ChineseYear s_knownYear = new ChineseYear();
+	
+		 yearDifference = ("unknownYear – s_knownYear")
+		 remainder = yearDifference % 12;
+
+	
+	case 0:
+		System.out.println(unknownYear + " is the year rat.");
+		break;
+		
+	case 1:
+		System.out.println(unknownYear + "is the year ox.");
+		break;
+		
+	case 2:
+		System.out.prinln (unknownYear + "is year of");
+		break;
+		
+
+
+
